cb71bf7b839faba9e0631b42c7a69c8209e7810e
harry obaseki <harryobas@gmail.com>
2020-02-05 16:04:22 +0100

    GET #index - returns success response

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,10 @@
 class AreasController < ApplicationController
+
+  def index
+    locations = Locations.all
+    render json: locations
+  end
+
+  def inside
+  end
 end
--- spec/controllers/areas_controller_spec.rb
+++ spec/controllers/areas_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AreasController, type: :controller do
+  let(:locations){File.read(File.expand_path("spec/fixtures/locations.json"))}
+
+  describe "GET #index" do
+    it "returns a success response" do
+      Locations.expects(:all).returns(locations)
+      get :index, params: {}
+      expect(response).to be_successful
+    end
+    
+  end
+
 
 end
